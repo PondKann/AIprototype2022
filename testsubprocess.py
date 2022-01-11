b7b8a302153475dd7e75a416e8435f0e2766d1c0
Pondkann <Pondkann@example.com>
2022-01-11 11:32:50 +0700

    ลอง

---
+++ testsubprocess.py
@@ -0,0 +1,6 @@
+import subprocess   #สำหรับ รัน terminal command
+
+if __name__ == "__main__":
+    subprocess.run(["ls", "-l"])
+    #subprocess.run(["python", "python_101.py", "9", "--x", "11", "--yval", "5" ])
+   
