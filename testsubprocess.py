363068820452473fb685b47f710f3242e0a845b7
Pondkann <Pondkann@example.com>
2022-01-11 11:49:53 +0700

    งานนะ

--- testsubprocess.py
+++ testsubprocess.py
@@ -1,6 +1,18 @@
-import subprocess   #สำหรับ รัน terminal command
+import subprocess #สำหรับ รัน terminal command
 
 if __name__ == "__main__":
-    subprocess.run(["ls", "-l"])
-    #subprocess.run(["python", "python_101.py", "9", "--x", "11", "--yval", "5" ])
-   
+    ## basic
+    subprocess.run(["ls","-l"])
+    for i in [2,5,6,8]:
+        subprocess.run(["python","python_101.py","9","--x", f"{i}", "--yval", "5"])
+
+
+    ## use output of subprocess
+    pro = subprocess.Popen(["ls","-l"],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = pro.communicate()
+    print(out)
+
+    ##HW ให้ print เฉพาะ ตัวเลขผลลัพธ์การคูณ
+    for i in [2,5,6,8]:
+        subprocess.run(["python","python_101.py","9","--x",f"{i}", "--yval", "5"],
+        stdout= subprocess.PIPE, stderr= subprocess.subprocess.PIPE)
